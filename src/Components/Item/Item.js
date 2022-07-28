e2106a38118bee35e6aabd576e4f71386b50e071
dmtrbb11 <dmitriy.bobrov.2001@mail.ru>
2022-07-28 20:33:02 +0400

    transition

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,13 +1,29 @@
 import styles from "./Item.module.css";
+import { useState } from "react";
 
 let Item = (props) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
-    <div className={styles.item}>
+    <div
+      className={styles.item}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
+    >
       <img className={styles.item_img} src="/img/item1.png" alt="" />
       <p className={styles.item_txt}>
         Увлажнитель воздуха STARWIND SHC1322, 3л, белый
       </p>
       <span className={styles.item_price}>1650 ₽</span>
+      {isHovering && <button className={styles.addToCard_btn}>добавить в корзину</button>}
     </div>
   );
 };
