494c9c709615dc74b47fc94cc979f943f827550f
dmtrbb11 <dmitriy.bobrov.2001@mail.ru>
2022-08-04 21:05:35 +0400

    инпуты и корзина адаптив

--- src/Components/CardItem/CardItem.js
+++ src/Components/CardItem/CardItem.js
@@ -1,22 +1,82 @@
+import React from "react";
 import styles from "./CardItem.module.css";
 
 let CardItem = (props) => {
+  const [count, setCount] = React.useState(0);
+  const minusCount = () => {
+    if (count > 0 && count <= 10) {
+      setCount(count - 1);
+    }
+  };
+  const plusCount = () => {
+    if (count <= 0 || count < 10) {
+      setCount(count + 1);
+    }
+  };
+  const deleteItem = () => {
+    props.setCardArr(
+      props.cardArr.filter((e) => {
+        return e.itemName !== props.itemName;
+      })
+    );
+    props.setCardCount(props.cardCount - 1);
+    console.log(props.cardArr);
+  };
   return (
-    <div className={styles.cardItem}>
-      <img className={styles.cardItem_img} src={props.imgURL} alt="" />
-      <p className={styles.cardItem_name}>{props.itemName}</p>
-      <div className={styles.counter}>
-        <button className={styles.counter_minus}></button>
-        <span className={styles.counter_value}>{1}</span>
-        <button className={styles.counter_plus}></button>
-      </div>
-      <span className={styles.cardItem_price}>{props.itemPrice}</span>
-      <div>
-        <button
-          style={{ backgroundImage: `url("/img/delete_btn.png")` }}
-          className={styles.deleteItem_btn}
-        ></button>
-      </div>
+    <div>
+      {window.innerWidth > 690 ? (
+        <div className={styles.cardItem}>
+          <img className={styles.cardItem_img} src={props.imgURL} alt="" />
+          <p className={styles.cardItem_name}>{props.itemName}</p>
+          <div className={styles.counter}>
+            <button
+              onClick={() => minusCount()}
+              className={styles.counter_minus}
+            ></button>
+            <span className={styles.counter_value}>{count}</span>
+            <button
+              onClick={() => plusCount()}
+              className={styles.counter_plus}
+            ></button>
+          </div>
+          <span className={styles.cardItem_price}>{props.itemPrice}</span>
+          <div>
+            <button
+              onClick={() => deleteItem()}
+              style={{ backgroundImage: `url("/img/delete_btn.png")` }}
+              className={styles.deleteItem_btn}
+            ></button>
+          </div>
+        </div>
+      ) : null}
+      {window.innerWidth <= 690 ? (
+        <div className={styles.cardItem}>
+          <img className={styles.cardItem_img} src={props.imgURL} alt="" />
+          <p className={styles.cardItem_name}>{props.itemName}</p>
+          <div className={styles.mobile_wraper}>
+            <span className={styles.cardItem_price}>{props.itemPrice}</span>
+            <div className={styles.counter}>
+              <button
+                onClick={() => minusCount()}
+                className={styles.counter_minus}
+              ></button>
+              <span className={styles.counter_value}>{count}</span>
+              <button
+                onClick={() => plusCount()}
+                className={styles.counter_plus}
+              ></button>
+            </div>
+            <div className={styles.deleteBtn}>
+              <button
+                onClick={() => deleteItem()}
+                className={styles.deleteItem_btn}
+              >
+                Удалить
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/Pages/CardPage/CardPage.js
+++ src/Pages/CardPage/CardPage.js
@@ -1,13 +1,121 @@
 import styles from "./CardPage.module.css";
 import CardItem from "../../Components/CardItem/CardItem";
 import React from "react";
+import InputMask from "react-input-mask";
 
 let CardPage = (props) => {
+  const [makeOrderClicked, setmakeOrderClicked] = React.useState(false);
+
+  const [nameInput, setNameInput] = React.useState("");
+  const [phoneInput, setPhoneInput] = React.useState("");
+  const [mailInput, setMailInput] = React.useState("");
+  const [nameCorrect, setNameCorrect] = React.useState(true);
+  const [phoneCorrect, setPhoneCorrect] = React.useState(true);
+  const [mailCorrect, setmailCorrect] = React.useState(true);
+
+  // сумма заказа
   let sum = 0;
   for (let i = 0; i < props.cardArr.length; i++) {
     sum = sum + parseInt(props.cardArr[i].itemPrice.replace(/[^0-9]/g, ""));
   }
-  console.log(sum);
+  //
+
+  const makeOrder = () => {};
+
+  // INPUT STYLES ////////////////////////
+
+  // const inputMailStyle = {
+  //   outlineColor: mailCorrect ? "#4892EC" : "#E86880",
+  //   outlineStyle: "solid",
+  //   outlineWidth: 1,
+  // };
+
+  // const inputEmptyStyle = {
+  //   outlineColor: "#E86880",
+  //   outlineStyle: "solid",
+  //   outlineWidth: 1,
+  // };
+  // const makeOrderBtnStyle = {
+  //   opacity:
+  //     nameInput !== "" && phoneInput !== "" && mailInput !== ""
+  //       ? "100%"
+  //       : "50%",
+  //   cursor:
+  //     nameInput !== "" && phoneInput !== "" && mailInput !== ""
+  //       ? "pointer"
+  //       : "default",
+  // };
+
+  ////////////////////////////////////////
+
+  // MAIL INPUT ////////////////////////
+  function isValidEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
+
+  const mailCorrectCheck = (event) => {
+    setMailInput(event.target.value);
+    if (!isValidEmail(event.target.value)) {
+      setmailCorrect(false);
+    } else {
+      setmailCorrect(true);
+    }
+  };
+  /////////////////////////////////////////
+
+  // NAME INPUT /////////////////////////
+  const nameInputComponent = function () {
+    if (nameCorrect === true) {
+      return (
+        <input
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+          className={styles.formInput_name}
+          placeholder={"Ваше имя"}
+        />
+      );
+    } else {
+      return (
+        <input
+          value={nameInput}
+          // style={inputEmptyStyle}
+          onChange={(e) => setNameInput(e.target.value)}
+          className={styles.formInput_name}
+          placeholder={"Ваше имя"}
+        />
+      );
+    }
+  };
+  ///////////////////////////////////////////
+  // TELEPHONE INPUT /////////////////////////
+  const telephoneInputComponent = function () {
+    if (phoneCorrect === true) {
+      return (
+        <InputMask
+          value={phoneInput}
+          onChange={(e) => setPhoneInput(e.target.value)}
+          className={styles.formInput_phone}
+          placeholder="+7(___) ___ - __ - __"
+          mask="+7(999) 999-99-99"
+          maskChar=" "
+        />
+      );
+    } else {
+      return (
+        <InputMask
+          value={phoneInput}
+          // style={inputEmptyStyle}
+          onChange={(e) => setPhoneInput(e.target.value)}
+          className={styles.formInput_phone}
+          placeholder="+7(___) ___ - __ - __"
+          mask="+7(999) 999-99-99"
+          maskChar=" "
+        />
+      );
+    }
+  };
+  ///////////////////////////////////////////
+
   return (
     <div>
       <div className="container">
@@ -34,13 +142,25 @@ let CardPage = (props) => {
       <section className={styles.form}>
         <div className={styles.form_wrapper}>
           <span className={styles.form_h1}>Пожалуйста, представьтесь</span>
-          <input className={styles.formInput_name} placeholder={"Ваше имя"} />
+          {nameInputComponent()}
+          {telephoneInputComponent()}
           <input
-            className={styles.formInput_phone}
-            placeholder="+7(___) ___ - __ - __"
+            value={mailInput}
+            onChange={mailCorrectCheck}
+            // style={
+            //   mailInput !== "" && mailCorrect === false ? inputMailStyle : null
+            // }
+            className={styles.formInput_mail}
+            placeholder={"Email"}
           />
-          <input className={styles.formInput_mail} placeholder={"Email"} />
-          <button className={styles.form_btn}>оформить заказ</button>
+
+          <button
+            onClick={() => makeOrder()}
+            // style={makeOrderBtnStyle}
+            className={styles.form_btn}
+          >
+            оформить заказ
+          </button>
         </div>
       </section>
     </div>
